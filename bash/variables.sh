fe912d033b73e315dd1703c06cee4a5f547fb923
guillaume-ggo <gourdier.g@gmail.com>
2017-06-05 18:56:42 +0200

    Variables substitution

---
+++ bash/variables.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+TARGET_ENV=$1
+if [ -z "${TARGET_ENV}" ]; then
+  echo "The TARGET_ENV variable is empty."
+  exit 1
+fi
+if [ ! -f ${TARGET_ENV}.properties ]; then
+  echo "The ${TARGET_ENV}.properties file does not exist."
+  exit 1
+fi
+source ${TARGET_ENV}.properties
+# We have to export variables to be used with envsubst command
+export TARGET_HOST
+# The following lines calls the envsubst command to write the final properties file
+envsubst < technical.properties.template > technical.properties
+source technical.properties
+if [ "${TARGET_HOST}" = "${HOST_NAME}" ]; then
+  echo "Variable substitution is ok."
+else
+  echo "TARGET_HOST=${TARGET_HOST} and HOST_NAME=${HOST_NAME}: different value"
+  exit 1
+fi
+
+exit 0
+
